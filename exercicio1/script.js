d9d1193a1c10b6c7c06ae13fcf18ad847c68977f
Maxuel Lima <maxuellima20@gmail.com>
2023-05-18 23:18:15 -0300

    Exercícios feitos individualmente de objetos

---
+++ exercicio1/script.js
@@ -0,0 +1,23 @@
+const sacolao = []
+
+const frutaUm = {
+    nome: "manga",
+    preco: 3.40,
+    disponibilidade: true
+}
+
+const frutaDois = {
+    nome: "maracuja",
+    preco: 9.90,
+    disponibilidade: false
+}
+
+const frutaTres = {
+    nome: "banana",
+    preco: 3.90,
+    disponibilidade: true
+}
+
+sacolao.push(frutaUm, frutaDois, frutaTres)
+console.log(sacolao)
+console.log(sacolao.length)
